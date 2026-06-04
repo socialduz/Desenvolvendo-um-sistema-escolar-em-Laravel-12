5e02580bcb48d0b4662b55c15267258c5db368b6
PerezVitor <vitor.g.perez@hotmail.com>
2026-06-04 12:37:44 -0300

    início criação dash board

---
+++ app/Http/Controllers/DashBoardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashBoardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('dashboard.index');
+    }
+
+}
+
+    /**
+     * Show the form for creating a new resource.
+     */
+  
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashBoardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashBoardController::class, 'index']);
