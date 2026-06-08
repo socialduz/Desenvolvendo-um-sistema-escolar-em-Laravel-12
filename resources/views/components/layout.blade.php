2d7786809af8c0f368f0d39034db2b1eb7a2553a
PerezVitor <vitor.g.perez@hotmail.com>
2026-06-08 08:26:50 -0300

    feat: cria componentes Blade reutilizáveis (Layout, Sidebar, Navbar, Footer, Modal, Script)

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <x-header/>
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+            <x-side-bar/>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+               <x-navbar/>
+
+                {{$slot}}
+                
+            </div>
+            <!-- End of Main Content -->
+
+          <x-footer/>
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <x-modal/>
+
+    <x-script/>
+    
+
+</body>
+
+</html>
